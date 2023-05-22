af079832ad9f0ab62f5628cb4b2a1ab6f4a62606
mishadiatel <mishadiatel13@gmail.com>
2023-05-22 18:55:06 +0300

    added pages for structure of website

---
+++ model/NewsArticles.ts
@@ -0,0 +1,15 @@
+
+export interface NewsArticle {
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage?: string;
+    publishedAt: string;
+    content: string;
+}
+
+export interface NewsResponse {
+    articles: NewsArticle[];
+
+}
