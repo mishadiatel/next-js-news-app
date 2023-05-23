b95ce985c642e166633f98035c6b42740acc2183
mishadiatel <mishadiatel13@gmail.com>
2023-05-23 17:09:13 +0300

    added 404 and 500 error pages

---
+++ pages/500.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>Error 🙃</h1>
+            <p>Looks like something went wrong. Please refresh the page or contact support!</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
