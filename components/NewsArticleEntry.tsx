cefc29d33a15f0e3b6ca8ef74c5a3e51e26c720e
mishadiatel <mishadiatel13@gmail.com>
2023-05-23 19:39:14 +0300

    fixed problem with images

--- components/NewsArticleEntry.tsx
+++ components/NewsArticleEntry.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import {NewsArticle} from "@/model/NewsArticles";
 import {Card} from "react-bootstrap";
+import Image from "next/image";
+import placeholderImage from '@/assets/images/newsarticle_placeholder.jpg'
+import styles from "@/styles/NewsArticleEntry.module.css";
+
 
 interface NewsArticleEntryProps {
     article: NewsArticle;
@@ -12,10 +16,18 @@ const NewsArticleEntry: React.FC<NewsArticleEntryProps> = ({article}) => {
         (urlToImage?.startsWith('http://') ||
             urlToImage?.startsWith('https://')) ?
             urlToImage : undefined;
+
     return (
         <a href={url}>
             <Card className={'h-100'}>
-                <Card.Img variant={'top'} src={validImageURL}/>
+                {/*<Card.Img variant={'top'} src={validImageURL || placeholderImage} alt={'News article image'}/>*/}
+                <Image
+                    src={validImageURL || placeholderImage}
+                    width={500}
+                    height={200}
+                    alt="News article image"
+                    className={`card-img-top ${styles.image}`}
+                />
                 <Card.Body>
                     <Card.Title>{title}</Card.Title>
                     <Card.Text>{description}</Card.Text>
